71d83976774134aba78dd28fba76a0405d4dcdbd
MrAhmedElsayed <ahmedsoftwaredevelopment@gmail.com>
2022-09-01 12:57:41 +0300

    front-end

--- quickstart/urls.py
+++ quickstart/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path, include
 from .views import home
 from rest_framework import routers
-from quickstart.views import ProductViewSet
+from quickstart.views import ProductViewSet, countries_list
 
 
 router = routers.DefaultRouter()
@@ -10,4 +10,5 @@ router.register(r'products', ProductViewSet)
 urlpatterns = [
     path("", home, name="home"),
     path('api/v1/', include(router.urls)),
+    path('api/v1/countries_list/', countries_list),
 ]
--- quickstart/views.py
+++ quickstart/views.py
@@ -3,6 +3,9 @@ from rest_framework import viewsets
 from .serializers import ProductSerializer
 from .models import Product
 from django_countries.data import COUNTRIES
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
 
 def home(request):
     return render(request, template_name="home,html")
@@ -12,3 +15,7 @@ class ProductViewSet(viewsets.ModelViewSet):
     queryset = Product.objects.all().order_by('-id')
     serializer_class = ProductSerializer
 
+
+@api_view(['GET'])
+def countries_list(request):
+    return Response(COUNTRIES)
